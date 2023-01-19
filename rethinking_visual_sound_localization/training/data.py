a7001e701e8ed04661139d197b47d51c4bc9c5f7
Aurora Cramer <aurora.linh.cramer@gmail.com>
2023-01-19 16:48:18 -0500

    Make sure threads is passed as a string

--- rethinking_visual_sound_localization/training/data.py
+++ rethinking_visual_sound_localization/training/data.py
@@ -270,7 +270,7 @@ class Ego4DDataset(IterableDataset):
                             sample_rate=self.sample_rate,
                             format='fltp',
                             decoder_option={
-                                "threads": 1,
+                                "threads": "1",
                             }
                         )
                         streamer.add_basic_video_stream(
@@ -278,7 +278,7 @@ class Ego4DDataset(IterableDataset):
                             frame_rate=self.fps,
                             format="rgb24",
                             decoder_option={
-                                "threads": 1,
+                                "threads": "1",
                             }
                         )
 
